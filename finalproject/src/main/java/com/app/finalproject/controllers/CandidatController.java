5a4a7d4a882fe1f1136dd4e271e145aabd8bcfb6
EricPuig10 <puigvendrelloetona@gmail.com>
2022-09-06 10:31:56 +0200

    hotfix crossorigin

--- finalproject/src/main/java/com/app/finalproject/controllers/CandidatController.java
+++ finalproject/src/main/java/com/app/finalproject/controllers/CandidatController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins="http://localhost:3000/")
+@CrossOrigin(origins="*")
 public class CandidatController {
 
 
