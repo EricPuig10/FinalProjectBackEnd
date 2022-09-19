49b68074eca72ffafe2c2dba10757cad04a2a5ac
[nboyaroca] <[nboyaroca@gmail.com]>
2022-09-19 17:08:47 +0200

    test service findByIdShouldReturnABootcampWithSameParamId

---
+++ finalproject/src/test/java/com/app/finalproject/services/BootcampServiceTest.java
@@ -0,0 +1,67 @@
+package com.app.finalproject.services;
+
+import com.app.finalproject.auth.facade.IAuthenticationFacade;
+import com.app.finalproject.models.Bootcamp;
+import com.app.finalproject.models.Category;
+import com.app.finalproject.models.User;
+import com.app.finalproject.repositories.IBootcampRepository;
+import com.app.finalproject.repositories.ICategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BootcampServiceTest {
+
+    @Mock
+    IBootcampRepository bootcampRepository;
+    IAuthenticationFacade authenticationFacade;
+    ICategoryRepository categoryRepository;
+
+    private IBootcampService bootcampService;
+
+    @BeforeEach
+    void beforeEach() {
+        this.bootcampService = new BootcampService(bootcampRepository, authenticationFacade, categoryRepository);
+    }
+
+    private Bootcamp createBootcamp() {
+        var authUser = new User();
+        authUser.setId(1L);
+
+        var category = new Category();
+        category.setId(1L);
+
+        var bootcamp = new Bootcamp();
+        bootcamp.setId(1L);
+        bootcamp.setBootcampName("bootcampName");
+        bootcamp.setDuration("duration");
+        bootcamp.setFormer("former");
+        bootcamp.setCategory(category);
+
+        return bootcamp;
+    }
+
+    @Test
+    void findByIdShouldReturnABootcampWithSameParamId() {
+        var bootcamp = this.createBootcamp();
+        var authUser = new User();
+        authUser.setId(1L);
+
+        Mockito.when(bootcampRepository.findById(any(Long.class))).thenReturn(Optional.of(bootcamp));
+
+        var sut = bootcampService.findById(1L, authUser);
+
+        assertThat(sut.getBootcampName(), equalTo(bootcamp.getBootcampName()));
+    }
+
+}
