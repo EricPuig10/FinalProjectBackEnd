7ed270fa8f4e9b13899b509faaf5dc3ad3124d83
[nboyaroca] <[nboyaroca@gmail.com]>
2022-09-21 11:01:40 +0200

    users working on postman

--- finalproject/src/main/java/com/app/finalproject/repositories/IUserRepository.java
+++ finalproject/src/main/java/com/app/finalproject/repositories/IUserRepository.java
@@ -1,7 +1,11 @@
 package com.app.finalproject.repositories;
 
+import com.app.finalproject.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface IUserRepository {
+public interface IUserRepository extends JpaRepository<User, Long> {
 }
