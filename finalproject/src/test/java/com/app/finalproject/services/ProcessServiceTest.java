7b1740e8a257ca4e97d1415d9fa3d8dbcb0a5b54
Laura Castillo <lauracasdu@gmail.com>
2022-09-24 07:36:01 +0200

    test Process service

--- finalproject/src/test/java/com/app/finalproject/services/ProcessServiceTest.java
+++ finalproject/src/test/java/com/app/finalproject/services/ProcessServiceTest.java
@@ -1,6 +1,7 @@
 package com.app.finalproject.services;
 
 import com.app.finalproject.auth.facade.IAuthenticationFacade;
+import com.app.finalproject.models.Candidat;
 import com.app.finalproject.models.ProcessState;
 import com.app.finalproject.models.User;
 import com.app.finalproject.repositories.IProcessStateRepository;
@@ -13,9 +14,12 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 
 @SpringBootTest
 class ProcessServiceTest {
@@ -33,6 +37,18 @@ class ProcessServiceTest {
         this.processService = new ProcessService(processStateRepository, authenticationFacade);
     }
 
+
+    private ProcessState createProcess() {
+        var authUser = new User();
+        authUser.setId(1L);
+
+        var process = new ProcessState();
+        process.setId(1L);
+
+        return process;
+    }
+
+
     @Test
     void getAllReturnsAListOfProcessState() {
 
@@ -46,4 +62,19 @@ class ProcessServiceTest {
 
         assertThat(sut.size(), equalTo(2));
     }
+
+
+    @Test
+    void findByIdShouldReturnABootcampWithSameParamId(){
+
+        var process = this.createProcess();
+        var authUser = new User();
+
+        Mockito.when(processStateRepository.findById(any(Long.class))).thenReturn(Optional.of(process));
+        var sut = processService.findById(1L, authUser);
+        assertThat(sut.getName(), equalTo(process.getName()));
+
+    }
+
 }
+
